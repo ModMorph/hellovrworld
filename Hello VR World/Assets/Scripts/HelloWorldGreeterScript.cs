61b00636fc0912f2895b64657a1a0f4058115a4e
Daniel Lesser <danlesser@live.com>
2016-02-18 17:08:12 -0700

    initial commit

---
+++ Hello VR World/Assets/Scripts/HelloWorldGreeterScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorldGreeterScript : MonoBehaviour {
+
+    public bool isWaving;
+
+    private Animator _animator; 
+	// Use this for initialization
+	void Start () {
+
+        _animator = GetComponent<Animator>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        _animator.SetBool("isWaving", isWaving);
+    }
+}
---
+++ Hello VR World/Assets/Scripts/WorldScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Rotate 1 degree around the y-axis every frame.
+        transform.Rotate(0f,1f,0f);
+
+	}
+}
